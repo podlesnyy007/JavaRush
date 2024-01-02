832164d7f9095afbc9f0a0c1fc83263ab278f1d8
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2024-01-03 00:29:26 +0400

    Create 03)Тип int – целые числа.java

---
+++ Java Syntax Pro/02)Знакомство с типами и ввод с клавиатуры/03)Тип int – целые числа.java
@@ -0,0 +1,20 @@
+/*
+В методе main объяви переменные intValue, numericValue, size, number типа int.
+Присвой им разные значения. Используй одну команду для создания и инициализации переменных.
+Значениями могут быть любые целые числа.
+Требования:
+•	В методе main объяви четыре переменные типа int.
+•	Имена переменных должны быть intValue, numericValue, size и number.
+•	Переменным сразу должны быть присвоены значения.
+•	Все переменные должны иметь разные значения.
+*/
+
+package com.javarush.tasks.pro.tasks02.task01;
+
+public class Solution 
+{
+    public static void main(String[] args) 
+    {
+        int intValue = 10, numericValue = 17, size = 109, number = 89;
+    }
+}
