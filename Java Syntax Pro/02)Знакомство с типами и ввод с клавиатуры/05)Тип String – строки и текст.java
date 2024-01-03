d679cf6474ebeff1b804b167b210422650b20e99
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2024-01-03 14:41:09 +0400

    Create 05)Тип String – строки и текст.java

---
+++ Java Syntax Pro/02)Знакомство с типами и ввод с клавиатуры/05)Тип String – строки и текст.java
@@ -0,0 +1,20 @@
+/*
+В методе main объяви переменные word, phrase, line и text типа String.
+Присвой им разные значения. Используй одну команду для создания и инициализации переменных.
+Значениями могут быть любые строки.
+Требования:
+•	В методе main объяви четыре переменные типа String.
+•	Назови переменные именами word, phrase, line и text.
+•	Переменным сразу присвой значения.
+•	У всех переменных должны быть разные значения.
+*/
+
+package com.javarush.tasks.pro.tasks02.task06;
+
+public class Solution 
+{
+    public static void main(String[] args) 
+    {
+        String word = "Hello", phrase = "! ", line = "My ", text = "Girl";
+    }
+}
