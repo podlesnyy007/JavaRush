38a40fc6a1d10b46c7f62d01df3c6fcfc4776408
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2024-01-03 19:57:05 +0400

    Create 06.1)Примеры работы со строками.java

---
+++ Java Syntax Pro/02)Знакомство с типами и ввод с клавиатуры/06.1)Примеры работы со строками.java
@@ -0,0 +1,24 @@
+/*
+В переменную tagline нужно записать строку "JustDoIt".
+Используй s1, s2, s3 и конкатенацию строк.
+Требования:
+•	Не изменяй значения переменных s1, s2, s3.
+•	Для инициализации переменной tagline используй s1, s2, s3 и конкатенацию строк.
+•	Программа должна выводить на экран значение переменной tagline.
+*/
+
+package com.javarush.tasks.pro.tasks02.task07;
+
+public class Solution 
+{
+    public static void main(String[] args) 
+    {
+        String s1 = "Do";
+        String s2 = "It";
+        String s3 = "Just";
+
+        String tagline = s3 + s1 + s2;
+
+        System.out.println(tagline);
+    }
+}
