07c4193c0253f76022eb3bc521fbe11991967519
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2024-01-03 13:55:14 +0400

    Create 04.1)Операции над переменными типа int.java

---
+++ Java Syntax Pro/02)Знакомство с типами и ввод с клавиатуры/04.1)Операции над переменными типа int.java
@@ -0,0 +1,24 @@
+/*
+В переменной centimeters записано количество сантиметров.
+В переменную meters нужно записать количество полных метров в centimeters (1 метр = 100 см).
+Для вычисления используй переменную centimeters и оператор деления.
+Для объявления и инициализации meters используй одну команду.
+Требования:
+•	Не изменяй значение переменной centimeters.
+•	Для вычисления meters должны использоваться centimeters и оператор деления.
+•	Программа должна выводить на экран значение переменной meters.
+*/
+
+package com.javarush.tasks.pro.tasks02.task02;
+
+public class Solution 
+{
+    public static void main(String[] args) 
+    {
+        int centimeters = 243;
+
+        int meters = centimeters / 100;
+
+        System.out.println(meters);
+    }
+}
