4c86ff2e793d632a4e95cc5a511305f1f5126fc4
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2024-01-03 20:40:31 +0400

    Create 06.3)Примеры работы со строками.java

---
+++ Java Syntax Pro/02)Знакомство с типами и ввод с клавиатуры/06.3)Примеры работы со строками.java
@@ -0,0 +1,24 @@
+/*
+В переменную digits нужно записать строку "60".
+Используй переменные x, y, z, пустую строку и конкатенацию строк.
+Требования:
+•	Не изменяй значения переменных x, y, z.
+•	Для инициализации переменной digits используй x, y, z, пустую строку и конкатенацию строк.
+•	Программа должна выводить на экран переменную digits.
+*/
+
+package com.javarush.tasks.pro.tasks02.task09;
+
+public class Solution 
+{
+    public static void main(String[] args) 
+    {
+        int x = 2;
+        int y = 4;
+        int z = 0;
+
+        String digits = x + y + (z + "");
+
+        System.out.println(digits);
+    }
+}
