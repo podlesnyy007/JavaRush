787d7f41bef39078538e1631e601146617c97c75
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2024-01-07 11:17:56 +0400

    Create 07.2)Лайфхаки: как писать код лучше и быстрее.java

---
+++ Java Syntax Pro/03)Условный оператор/07.2)Лайфхаки: как писать код лучше и быстрее.java
@@ -0,0 +1,30 @@
+/*
+Эта программа написана с использованием тернарного оператора, но не совсем понятно, что она делает. 
+Думаем, если избавиться от тернарного оператора, то станет намного понятнее.
+Перепиши программу без использования тернарного оператора.
+Требования:
+•	Программа должна считывать число c клавиатуры.
+•	Вместо тернарного оператора в программе должен быть использован оператор if.
+•	Функционал программы не должен измениться.
+*/
+
+package com.javarush.task.pro.task03.task0310;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        /*
+        String result = number < 5 ? "число меньше 5" : number > 5 ? "число больше 5" : "число равно 5";
+        System.out.println(result);
+        */
+        if (number < 5)
+            System.out.println("число меньше 5");
+        else if (number > 5)
+            System.out.println("число больше 5");
+        else
+            System.out.println("число равно 5");
+    }
+}
