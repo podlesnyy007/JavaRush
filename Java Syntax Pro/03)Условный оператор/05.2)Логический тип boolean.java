490ad39b8c0d838dcbc3babbd067431437872594
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2024-01-07 11:57:41 +0400

    Update 05.2)Логический тип boolean.java

--- Java Syntax Pro/03)Условный оператор/05.2)Логический тип boolean.java
+++ Java Syntax Pro/03)Условный оператор/05.2)Логический тип boolean.java
@@ -16,19 +16,21 @@ package com.javarush.task.pro.task03.task0305;
 
 import java.util.Scanner;
 
-public class Solution 
+public class Solution
 {
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
         Scanner console = new Scanner(System.in);
         int a = console.nextInt();
         int b = console.nextInt();
         int c = console.nextInt();
-        
-        if (a == b && b == c)
-            System.out.println(a + " " + b + " " + c);
-        else if (a == b)
-            System.out.println(a + " " + b);
+
+        if (a == b) {
+            if (b == c)
+                System.out.println(a + " " + b + " " + c);
+            else
+                System.out.println(a + " " + b);
+        }
         else if (b == c)
             System.out.println(b + " " + c);
         else if (a == c)
