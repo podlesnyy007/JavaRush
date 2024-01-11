8df6fb11a44998ecb38e1345e0cac8e7fb4cbc63
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2024-01-11 22:07:34 +0400

    Create 01.01)Массивы.java

---
+++ 01)Java Syntax Pro/06)Массивы/01.01)Массивы.java
@@ -0,0 +1,15 @@
+/*
+В методе main проинициализируй переменные intArray и doubleArray массивами соответствующих типов, размер которых равен 10.
+Требования:
+•	В методе main(String[]) проинициализируй переменную intArray согласно условию.
+•	В методе main(String[]) проинициализируй переменную doubleArray согласно условию.
+*/
+
+package com.javarush.task.pro.task05.task0501;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] intArray = new int[10];
+        double[] doubleArray = new double[10];
+    }
+}
