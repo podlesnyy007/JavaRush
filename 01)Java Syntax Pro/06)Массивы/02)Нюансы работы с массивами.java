076dfda8c965f64bcab33a2e5ce5642e790abaf1
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2024-01-11 22:55:49 +0400

    Create 02)Нюансы работы с массивами.java

---
+++ 01)Java Syntax Pro/06)Массивы/02)Нюансы работы с массивами.java
@@ -0,0 +1,31 @@
+/*
+Реализовать метод main(String[]), в котором нужно скопировать содержимое массивов firstArray и secondArray в один массив resultArray.
+Массив firstArray должен быть в начале нового массива resultArray, а secondArray — после него.
+Программа должна правильно обрабатывать массивы любой длины.
+Требования:
+•	В классе Solution должна быть публичная статическая переменная firstArray типа int[], проинициализированная массивом с 10-ю значениями.
+•	В классе Solution должна быть публичная статическая переменная secondArray типа int[], проинициализированная массивом с 10-ю значениями.
+•	В классе Solution должна быть публичная статическая переменная resultArray типа int[].
+•	Реализуй метод main(String[]) согласно условию.
+*/
+
+package com.javarush.task.pro.task05.task0504;
+
+public class Solution {
+    public static int[] firstArray = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    public static int[] secondArray = new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+    public static int[] resultArray;
+
+    public static void main(String[] args) {
+        int lengthArray = firstArray.length + secondArray.length;
+        resultArray = new int[lengthArray];
+        for (int i = 0; i < firstArray.length; i++)
+            resultArray[i] = firstArray[i];
+        for (int i = 0; i < secondArray.length; i++)
+            resultArray[i + firstArray.length] = secondArray[i];
+
+        for (int i = 0; i < resultArray.length; i++) {
+            System.out.print(resultArray[i] + ", ");
+        }
+    }
+}
