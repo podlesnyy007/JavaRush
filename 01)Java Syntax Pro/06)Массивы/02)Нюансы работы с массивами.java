0e220850d5384a3b3bab5a1bcbffbfad4b150995
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2024-01-12 11:19:52 +0400

    Update 02)Нюансы работы с массивами.java

--- 01)Java Syntax Pro/06)Массивы/02)Нюансы работы с массивами.java
+++ 01)Java Syntax Pro/06)Массивы/02)Нюансы работы с массивами.java
@@ -11,6 +11,10 @@
 
 package com.javarush.task.pro.task05.task0504;
 
+/* 
+Объединяем массивы
+*/
+
 public class Solution {
     public static int[] firstArray = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     public static int[] secondArray = new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
