9f6fc8021c0cd8fe2f08b1a0a746640b901098e0
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2024-01-12 20:36:36 +0400

    Create 08.03)Класс Arrays, часть 2.java

---
+++ 01)Java Syntax Pro/06)Массивы/08.03)Класс Arrays, часть 2.java
@@ -0,0 +1,24 @@
+/*
+Реализуй метод main(String[]), который сортирует массив array по возрастанию.
+Для сортировки массива используй метод Arrays.sort(int[]).
+Требования:
+•	Реализуй метод main(String[]) согласно условию.
+*/
+
+package com.javarush.task.pro.task05.task0518;
+
+import java.util.Arrays;
+
+/* 
+Сортировка массива
+*/
+
+public class Solution {
+
+    public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+    public static void main(String[] args) {
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
