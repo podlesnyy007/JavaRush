b48aef2d885e1ccde3d830e8ffc50bb811b067ca
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2024-01-12 11:20:22 +0400

    Update 03.01)Примеры использования массивов.java

--- 01)Java Syntax Pro/06)Массивы/03.01)Примеры использования массивов.java
+++ 01)Java Syntax Pro/06)Массивы/03.01)Примеры использования массивов.java
@@ -16,6 +16,10 @@ package com.javarush.task.pro.task05.task0505;
 
 import java.util.Scanner;
 
+/* 
+Reverse
+*/
+
 public class Solution {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
