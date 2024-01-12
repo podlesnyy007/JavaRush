4aaaab62015801b992959cc706ac37987f1748f0
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2024-01-12 11:18:06 +0400

    Update 01.01)Массивы.java

--- 01)Java Syntax Pro/06)Массивы/01.01)Массивы.java
+++ 01)Java Syntax Pro/06)Массивы/01.01)Массивы.java
@@ -7,6 +7,10 @@
 
 package com.javarush.task.pro.task05.task0501;
 
+/* 
+Создаем массивы
+*/
+
 public class Solution {
     public static void main(String[] args) {
         int[] intArray = new int[10];
