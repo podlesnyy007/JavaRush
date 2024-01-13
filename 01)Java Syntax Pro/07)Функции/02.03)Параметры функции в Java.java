e5d6cec80f6b4442aa19a900fafd7483fc029bb4
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2024-01-13 22:21:38 +0400

    Create 02.03)Параметры функции в Java.java

---
+++ 01)Java Syntax Pro/07)Функции/02.03)Параметры функции в Java.java
@@ -0,0 +1,44 @@
+/*
+Напишем утилиту для работы с массивами. Основная часть функционала готова: метод printArray() выводит в консоли все элементы массива.
+Тебе осталась мелочь: реализовать метод reverseArray(). Он должен менять порядок элементов массива на обратный.
+Метод должен работать только с массивами целочисленных значений (int[]).
+Пример:
+Если массив содержал элементы:
+1, 2, 3, 4, 5, 6, 7, 8, 9, 0
+то после вызова метода reverseArray() должен содержать:
+0, 9, 8, 7, 6, 5, 4, 3, 2, 1
+Требования:
+•	Метод reverseArray() должен менять порядок элементов массива на обратный.
+*/
+
+package com.javarush.task.pro.task06.task0605;
+
+/* 
+Правильный порядок
+*/
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] array = {11, 22, 33, 44, 55, 66, 77, 88, 99};
+        printArray(array);
+        reverseArray(array);
+        printArray(array);
+    }
+
+    public static void reverseArray(int[] array) {
+        int[] copy = Arrays.copyOf(array, array.length);
+        for (int i = 0; i < array.length; i++) {
+            array[i] = copy[array.length - 1 - i];
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+}
