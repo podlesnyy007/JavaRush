eb997a00c44e14b1265021e641afd0ed2f96f6d3
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2024-01-13 22:41:22 +0400

    Create 02.04)Параметры функции в Java.java

---
+++ 01)Java Syntax Pro/07)Функции/02.04)Параметры функции в Java.java
@@ -0,0 +1,61 @@
+/*
+Неплохо было бы иметь один метод для решения различных задач. У тебя есть возможность написать такой.
+Создай еще 9 методов universalMethod(). В сумме их должно получиться 10.
+Параметры, которые они должны принимать, придумай сам.
+
+Требования:
+•	Программа должна содержать 10 методов с именем universalMethod.
+*/
+
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+
+    public static void universalMethod(int x){
+
+    }
+
+    public static void universalMethod(double y){
+
+    }
+
+    public static void universalMethod(String str){
+
+    }
+
+    public static void universalMethod(int[] array){
+
+    }
+
+    public static void universalMethod(double[][] mas){
+
+    }
+
+    public static void universalMethod(boolean bool){
+
+    }
+
+    public static void universalMethod(int a, int b){
+
+    }
+
+    public static void universalMethod(int x, double y){
+
+    }
+
+    public static void universalMethod(String str, double n){
+
+    }
+}
