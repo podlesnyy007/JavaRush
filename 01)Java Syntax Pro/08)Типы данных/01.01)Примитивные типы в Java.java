65bc52e5378f1205e8e6a40a895cdf27563d266a
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2024-01-21 13:48:59 +0400

    Create 01.01)Примитивные типы в Java.java

---
+++ 01)Java Syntax Pro/08)Типы данных/01.01)Примитивные типы в Java.java
@@ -0,0 +1,27 @@
+/*
+Как тебе уже известно, всего в языке Java 8 примитивных типов.
+Создай по одной переменной каждого примитивного типа в классе Solution.
+Переменные не должны находиться в каком-либо методе.
+Требования:
+•	В классе Solution должны быть созданы переменные всех примитивных типов.
+*/
+
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    byte intNumber1;
+    short intNumber2;
+    int intNumber3;
+    long intNumber4;
+
+    float floatNumber1;
+    double floatNumber2;
+
+    boolean bool;
+
+    char symbol;
+}
