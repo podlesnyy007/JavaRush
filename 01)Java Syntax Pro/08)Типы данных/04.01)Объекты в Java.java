773a4cbbd26e36b471a92f89b79a9924eaa52b65
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2024-01-22 13:51:02 +0400

    Create 04.01)Объекты в Java.java

---
+++ 01)Java Syntax Pro/08)Типы данных/04.01)Объекты в Java.java
@@ -0,0 +1,44 @@
+/*
+Используя оператор new в методе main(), создай:
+10 объектов типа String;
+5 объектов типа int[] (массивов значений int);
+2 объекта типа Scanner.
+Требования:
+•	В методе main() должно создаваться 10 объектов типа String.
+•	В методе main() должно создаваться 5 объектов типа int[].
+•	В методе main() должно создаваться 2 объекта типа Scanner.
+*/
+
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Ctrl + D -> дублирует строку на которой стоит курсор
+
+        String str1 = new String();
+        String str2 = new String();
+        String str3 = new String();
+        String str4 = new String();
+        String str5 = new String();
+        String str6 = new String();
+        String str7 = new String();
+        String str8 = new String();
+        String str9 = new String();
+        String str10 = new String();
+
+        int[] array1 = new int[0];
+        int[] array2 = new int[0];
+        int[] array3 = new int[0];
+        int[] array4 = new int[0];
+        int[] array5 = new int[0];
+
+        Scanner console1 = new Scanner("");
+        Scanner console2 = new Scanner("");
+    }
+}
